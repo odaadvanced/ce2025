0425c4d75ce250306b8608adf75acf09cc5250cc
Del <stbaoda@gmail.com>
2025-12-06 10:37:22 -0600

    updated the code

--- user/stop_lights.py
+++ user/stop_lights.py
@@ -1,38 +1,60 @@
 from gpiozero import Button, LED, Device
-from gpiozero import TonalBuzzer
-from gpiozero.tones import Tone
+import RPi.GPIO as GPIO
+
+buzzer_bcm = 16
+red_bcm = 17
+green_bcm = 22
+yellow_bcm = 4
+button_bcm = 18
+
 from signal import pause
 import time
 from oled_io import Oled_io
 
-display = Oled_io()
-beep = TonalBuzzer(16)
+def setup():
+    global Buzz                     # Assign a global variable to replace GPIO.PWM
+    global red_light, green_light, yellow_light, button, display, light, is_pressed
 
-red_light = LED(17)
-green_light = LED(22)
-yellow_light = LED(4)
+    display = Oled_io()
+    GPIO.setmode(GPIO.BCM)        # Numbers GPIOs by BCM number
+    GPIO.setup(buzzer_bcm, GPIO.OUT)    # Set pins' mode is output
+    Buzz = GPIO.PWM(buzzer_bcm, 440)    # 440 is initial frequency.
 
-button = Button(18)
-is_pressed = False
-light = 0
+    red_light = LED(red_bcm)
+    green_light = LED(green_bcm)
+    yellow_light = LED(yellow_bcm)
 
+    button = Button(button_bcm)
+    is_pressed = False
+    light = 0
+    button.when_released = on_release
+    turn_lights_off()
+    set_red_on()
+ 
 def beep_display(text, light_wait, length):
     global beep, light
-    display.print(text)
-    if light != 0:
-        for count in range(light_wait):
-            beep.play(Tone("A4"))
+
+    for count in range(light_wait):
+        display.print(text + '\n ' + str(light_wait - count))
+        if light != 0:
+            Buzz.start(50)
             time.sleep(length/2)
-            beep.stop()
+            Buzz.stop()
             time.sleep(length/2)
-    else:
-        time.sleep(length * light_wait)
+        else:
+            time.sleep(length)
                   
+def beep_display_red(text, light_wait, length):
+    global beep, light
+    for count in range(light_wait):
+        display.print(text + '\n ' + str(light_wait - count))
+        time.sleep(length)
+                   
 
 def set_red_on():
     global light, is_pressed
     red_light.off()
-    beep_display ("DO NOT CROSS", 15, 0.5)
+    display.print ("DO NOT CROSS")
     is_pressed = False
         
 def turn_lights_off():
@@ -44,7 +66,7 @@ def red_light_on():
     global light
     turn_lights_off()
     red_light.off()
-    beep_display ("DO NOT CROSS", 15, 0.0)
+    beep_display ("DO NOT CROSS", 15, 0.5)
     light = light + 1
     green_light_on()
         
@@ -66,7 +88,7 @@ def green_light_on():
     yellow_light_on()
        
     
-def on_press():
+def on_release():
     global  is_pressed
     
     if is_pressed == True:
@@ -75,7 +97,6 @@ def on_press():
     display_lights()
     
     
-
 def display_lights():
     global light, is_pressed
     
@@ -88,27 +109,20 @@ def display_lights():
         green_light_on()
         
     elif light == 2:
-        yellow_light_on()
-    
-  
+        yellow_light_on() 
    
-def on_release():
-    led.off()
-    print ("button release")
-
-
-
-try:
-    button.when_pressed = on_press
-    turn_lights_off()
-    set_red_on()
-    pause()
-finally:
-    print ("closing down")
-    beep.close()
+def destroy():
+ 
     red_light.close()
     green_light.close()
     yellow_light.close()
     button.close()
-    Device.close()
-    
+    Buzz.stop()                 # Stop the buzzer
+    GPIO.output(buzzer_bcm, 1)      # Set Buzzer pin to High
+    GPIO.cleanup()              # Release resource
+
+try:
+    setup()
+    pause()
+except KeyboardInterrupt:   # When 'Ctrl+C' is pressed, the program destroy() will be  executed.
+    destroy()
